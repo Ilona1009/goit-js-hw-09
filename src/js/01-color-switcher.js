ec50f9f7c25a4ef4b710c31c1d2a3647bdc1de75
ilona1009 <maliarenko.ilona@gmail.com>
2023-04-11 23:40:30 +0200

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startRef = document.querySelector('button[data-start]');
+const stopRef = document.querySelector('button[data-stop="stop"]');
+const bodyRef = document.querySelector('body');
+
+startRef.addEventListener('click', clickbtn);
+
+let timeId;
+function clickbtn() {
+    timeId = setInterval(switchColor, 1000);
+
+
+    if (timeId) {
+        return;
+    }
+}
+
+
+function switchColor() {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
