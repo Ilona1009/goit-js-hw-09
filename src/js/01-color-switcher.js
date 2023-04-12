3c56bfd966abb5924d2930783a4ee7a180aa3f55
ilona1009 <maliarenko.ilona@gmail.com>
2023-04-12 10:24:02 +0200

    1 hometask

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,28 @@
 const startRef = document.querySelector('button[data-start]');
-const stopRef = document.querySelector('button[data-stop="stop"]');
+const stopRef = document.querySelector('button[data-stop]');
 const bodyRef = document.querySelector('body');
 
+let timeId = null;
 startRef.addEventListener('click', clickbtn);
+stopRef.addEventListener('click', stopSwitch);
 
-let timeId;
+
+
+
 function clickbtn() {
-    timeId = setInterval(switchColor, 1000);
+    timeId = setInterval(() => { return bodyRef.style.backgroundColor = getRandomHexColor() }, 1000);
+    startRef.setAttribute('disabled', '');
 
-
-    if (timeId) {
-        return;
-    }
-}
-
-
-function switchColor() {
-    bodyRef.style.backgroundColor = getRandomHexColor();
 }
 
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+function stopSwitch() {
+    clearInterval(timeId);
+    startRef.removeAttribute('disabled');
 }
 
