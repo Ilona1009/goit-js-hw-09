71adf6d7beb6d817487545a351848c74f043f5c7
Ilona1009 <105588398+Ilona1009@users.noreply.github.com>
2023-04-12 08:33:02 +0000

    Deploying to gh-pages from @ Ilona1009/goit-js-hw-09@3c56bfd966abb5924d2930783a4ee7a180aa3f55 🚀

---
+++ 01-color-switcher.c677cffd.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),r=null;t.addEventListener("click",(function(){r=setInterval((function(){return n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.c677cffd.js.map
